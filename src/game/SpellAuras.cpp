f69599893ed88c4f66e5467a786c0470d678c828
Brueggus <brueggus@orangevir.us>
2010-11-02 11:06:59 +0100

    Revert "[OV35] Implement Anticheat-Patch by Gimli, KAPATEJIb and others"
    This reverts commit a6f3e1d017c837f69ebe85201fa15ab76d2329d0.

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -4453,17 +4453,10 @@ void Aura::HandleAuraModIncreaseFlightSpeed(bool apply, bool Real)
     {
         WorldPacket data;
         if(apply)
-        {
-            ((Player*)GetTarget())->SetCanFly(true);
             data.Initialize(SMSG_MOVE_SET_CAN_FLY, 12);
-        }
         else
-        {
             data.Initialize(SMSG_MOVE_UNSET_CAN_FLY, 12);
-            ((Player*)target)->SetCanFly(false);
-        }
-        //data.append(GetTarget()->GetPackGUID());
-        data << target->GetPackGUID();
+        data << m_target->GetPackGUID();
         data << uint32(0);                                      // unknown
         target->SendMessageToSet(&data, true);
 
@@ -6487,18 +6480,10 @@ void Aura::HandleAuraAllowFlight(bool apply, bool Real)
     // allow fly
     WorldPacket data;
     if(apply)
-    {
-        if (GetTarget()->GetTypeId() == TYPEID_PLAYER)
-            ((Player*)GetTarget())->SetCanFly(true);
         data.Initialize(SMSG_MOVE_SET_CAN_FLY, 12);
-    }
     else
-    {
         data.Initialize(SMSG_MOVE_UNSET_CAN_FLY, 12);
-        if (GetTarget()->GetTypeId() == TYPEID_PLAYER)
-            ((Player*)GetTarget())->SetCanFly(false);
-    }
-    data << GetTarget()->GetPackGUID();
+    data << m_target->GetPackGUID();
     data << uint32(0);                                      // unk
     GetTarget()->SendMessageToSet(&data, true);
 }
