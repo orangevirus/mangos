ed919b4c8ca95a40c57fe0c54c38dffcac6d7e15
[DEV]kelthuzad <kel_thalas@web.de>
2011-01-29 15:44:37 +0100

    [OV159] remove forcefield from SOTA defenders spawn zone

--- sql/custom/159_01_mangos_strand_of_the_ancients.sql
+++ sql/custom/159_01_mangos_strand_of_the_ancients.sql
@@ -1,10 +1,13 @@
--- strand of the ancients update to make vehicles work
+-- strand of the ancients 
 
+-- update to make vehicles work
 -- Demolisher
 UPDATE `creature_template` SET `npcflag` = '1', minhealth = '80000', maxhealth = '80000', unit_flags = 32768, dynamicflags = 8, spell1 = 52338, spell2 = 60206, mechanic_immune_mask = 67110928, `ScriptName` = 'npc_sa_demolisher' WHERE `entry` = '28781';
-
 -- AP Cannon
 UPDATE `creature_template` SET `npcflag` = '1', unit_flags = '4108', minhealth = '60000', maxhealth = '60000', `faction_A` = '35', `faction_H` = '35', dynamicflags = 8, spell1 = 49872, mechanic_immune_mask = 67110928, flags_extra = 2, `ScriptName` = 'npc_sa_cannon' WHERE entry = '27894';
+
+-- remove forcefield at defenders spawn
+UPDATE `gameobject` SET `spawnMask` = '0' WHERE `guid` = 300086;
 
 -- translate text to german
 UPDATE `mangos_string` SET `content_loc3` = 'Allianz' where entry = 650;
