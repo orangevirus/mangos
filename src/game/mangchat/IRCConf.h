16cf8b1bf12bfd43ef61b91bf43f3cf0f34442ee
pasdVn <pasdVn3@gmx.de>
2011-03-08 19:22:03 +0100

    A bit of cleanup for the cmake build system.

--- src/game/mangchat/IRCConf.h
+++ src/game/mangchat/IRCConf.h
@@ -3,10 +3,6 @@
 
 #include "Platform/CompilerDefs.h"
 
-// Format is YYYYMMDDRR where RR is the change in the conf file
-// for that day.
-#define MANGCHAT_CONF_VERSION    2008011901
-
 #if PLATFORM == PLATFORM_WINDOWS
   #define _MANGCHAT_CONFIG  "mangchat.conf"
 #else
