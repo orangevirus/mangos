0fb4bff9db5132fa161c8582f8cce64f40e82d4c
tibbi <tibbi@centrum.sk>
2011-04-08 17:08:54 +0200

    [IOV197] remove negative auras at leaving Battleground
    Signed-off-by: Janu <jaed1@gmx.net>

--- src/game/BattleGround.cpp
+++ src/game/BattleGround.cpp
@@ -1165,10 +1165,11 @@ void BattleGround::RemovePlayerAtLeave(ObjectGuid guid, bool Transport, bool Sen
 
             if (!team) team = plr->GetTeam();
 
+            plr->RemoveArenaAuras(true);                // removes debuffs / dots etc., we don't want the player to die after porting out
+
             // if arena, remove the specific arena auras
             if (isArena())
             {
-                plr->RemoveArenaAuras(true);                // removes debuffs / dots etc., we don't want the player to die after porting out
                 bgTypeId=BATTLEGROUND_AA;                   // set the bg type to all arenas (it will be used for queue refreshing)
 
                 // unsummon current and summon old pet if there was one and there isn't a current pet
