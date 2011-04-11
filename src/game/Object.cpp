2b74bd4a50019c139ca3a831269445ab19cbf3c3
[DEV]kelthuzad <kel_thalas@web.de>
2011-04-12 00:01:40 +0200

    fixed project files and compile errors
    Signed-off-by: x3n <system.x3n@gmail.com>
    
    Conflicts:
    
    	win/VC100/game.vcxproj.filters

--- src/game/Object.cpp
+++ src/game/Object.cpp
@@ -2304,6 +2304,7 @@ GameObject* WorldObject::GetClosestGameObjectWithEntry(WorldObject* pSource, uin
     cell.Visit(p, grid_gobject_searcher,*(pSource->GetMap()), *this, fMaxSearchRange);
 
     return pGameObject;
+}
 
 bool WorldObject::PrintCoordinatesError(float x, float y, float z, char const* descr) const
 {
