a81973787c42c8b3f3f4bc9383570d195698b336
Ancient <anubis@abwesend.de>
2011-04-28 00:45:59 +0200

    [IOV243] Shiv should award 1 combo point
    Signed-off-by: Janu <jaed1@gmx.net>

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -2895,6 +2895,8 @@ void Spell::EffectDummy(SpellEffectIndex eff_idx)
                     Item *item = pCaster->GetWeaponForAttack(OFF_ATTACK);
                     if (!item)
                         return;
+                    
+                    pCaster->AddComboPoints(unitTarget, 1);
 
                     // all poison enchantments is temporary
                     uint32 enchant_id = item->GetEnchantmentId(TEMP_ENCHANTMENT_SLOT);
